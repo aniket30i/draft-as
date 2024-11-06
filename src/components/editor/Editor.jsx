bb06ed9b706b75efc846631b2aad6d10bdec4593
Aniket Saha <iamaniket.saha.09@protonmail.com>
2024-11-06 13:21:24 +0530

    title and button placement and designs

---
+++ src/components/editor/Editor.jsx
@@ -0,0 +1,14 @@
+import "./Editor.css";
+
+const Editor = () => {
+  return (
+    <div className="editor-container">
+      <div className="btn-container">
+        <button className="btn-save">Save</button>
+      </div>
+      <div className="editor-space"></div>
+    </div>
+  );
+};
+
+export default Editor;
